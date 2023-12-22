2928654f6dd1f7b6538c1959ecfb3dab609e5cfb
Jankulovska <jankulovskasara60@gmail.com>
2023-12-22 10:13:28 +0100

    Add first java program

---
+++ src/zadacaaa43/Student.java
@@ -0,0 +1,12 @@
+package zadacaaa43;
+//A Java class which has only setter methods. 
+public class Student {
+	   
+	//private data member  
+	private String college;  
+	//getter method for college  
+	public void setCollege(String college){  
+	this.college=college;  
+	}  
+
+}
